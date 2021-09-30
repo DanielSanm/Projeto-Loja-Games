a1b8c0c3e87af4fdaed382bf309617865fd0bda6
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-30 19:14:54 -0300

    [Feature Gustavo - Task: Importando classes da Model]

--- src/main/java/com/generation/LojaGames/Repository/RepositoryProduto.java
+++ src/main/java/com/generation/LojaGames/Repository/RepositoryProduto.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.generation.LojaGames.Model.Produtos;
+
 @Repository
 public interface RepositoryProduto extends JpaRepository<Produtos, Long>{
 
