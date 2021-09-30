ce5ed979222ea79f787afa6592f493258e5b3484
GScabuzzi <gustavo-gsg@hotmail.com>
2021-09-30 16:24:00 -0300

    [Feature Gustavo - Task: ADD Class RepositoryCategoria and RepositoryProduto]

--- src/main/java/com/generation/LojaGames/Repository/RepositoryCategoria.java
+++ src/main/java/com/generation/LojaGames/Repository/RepositoryCategoria.java
@@ -1,5 +1,21 @@
 package com.generation.LojaGames.Repository;
 
-public interface RepositoryCategoria {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryCategoria extends JpaRepository<Categoria, Long>{
+	
+	/**
+	 * 
+	 * 
+	 * @param descricaoCategoria
+	 * @return Lista com todos as descriçoes que contenham a palavra digitada
+	 * @author Gustavo
+	 * @since 1.0
+	 */
+	public List<Categoria> findAllByDescricaoCategoriaContainingIgnoreCase(String descricaoCategoria);
 
 }
