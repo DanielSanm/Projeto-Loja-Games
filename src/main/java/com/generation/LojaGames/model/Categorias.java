d287b44cc5f76cf2e99d2867d48fff66ece998f4
DanielSanm <danielelias3221@gmail.com>
2021-09-30 15:56:47 -0300

    [Feature Daniel - Task: Criando classe model]

---
+++ src/main/java/com/generation/LojaGames/model/Categorias.java
@@ -0,0 +1,47 @@
+package com.generation.LojaGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categorias {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private@Id Long idCategoria;
+	
+	@NotBlank
+	private String nomeCategoria;
+	
+	@NotBlank
+	private String setor;
+	
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+
+}
---
+++ src/main/java/com/generation/LojaGames/model/Produtos.java
@@ -0,0 +1,69 @@
+package com.generation.LojaGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private @Id Long idProduto;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private Double preco;
+	
+	@NotBlank
+	private String tipoProduto;
+	
+	private Long fkCategoria;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(String tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public Long getFkCategoria() {
+		return fkCategoria;
+	}
+
+	public void setFkCategoria(Long fkCategoria) {
+		this.fkCategoria = fkCategoria;
+	}
+
+}
