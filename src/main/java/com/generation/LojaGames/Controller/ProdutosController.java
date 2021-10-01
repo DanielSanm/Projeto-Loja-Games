e2d5bf9a545a96e0038b7d126eb306bcf1b6d0c1
abnerwerley <abnerwerley77@gmail.com>
2021-10-01 09:54:04 -0300

    [Feature Abner - Task: métodos adicionados em ProdutosRepositorio, e ProdutosController adicionado]

--- src/main/java/com/generation/LojaGames/Controller/ProdutosController.java
+++ src/main/java/com/generation/LojaGames/Controller/ProdutosController.java
@@ -1,5 +1,66 @@
+
 package com.generation.LojaGames.Controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.LojaGames.Model.Produtos;
+import com.generation.LojaGames.repository.ProdutoRepositorio;
+
+@RestController
+@RequestMapping("/api/v1/produtos")
+@CrossOrigin("*")
 public class ProdutosController {
 
+	@Autowired
+	private ProdutoRepositorio repositorio;
+
+	@GetMapping("/{id_produto}")
+
+	public ResponseEntity<Produtos> GetById(@PathVariable(value = "id_produto") Long idProduto) {
+		Optional<Produtos> optional = repositorio.findById(idProduto);
+
+		if (optional.isPresent()) {
+			return ResponseEntity.status(200).body(optional.get());
+		} else {
+			return ResponseEntity.status(204).build();
+		}
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Produtos> salvar(@RequestBody Produtos produto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositorio.save(produto));
+	}
+
+	@PutMapping("/atualizar")
+	public ResponseEntity<Produtos> atualizar(@Valid @RequestBody Produtos novoProduto) {
+		return ResponseEntity.status(200).body(repositorio.save(novoProduto));
+	}
+
+	@DeleteMapping("/deletar")
+	public ResponseEntity<Produtos> deletar(@PathVariable(value = "id_produto") Long IdPostagem) {
+		Optional<Produtos> optional = repositorio.findById(IdPostagem);
+
+		if (optional.isPresent()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(400).build();
+
+		}
+
+	}
 }
--- src/main/java/com/generation/LojaGames/repository/ProdutoRepositorio.java
+++ src/main/java/com/generation/LojaGames/repository/ProdutoRepositorio.java
@@ -1,6 +1,7 @@
 package com.generation.LojaGames.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.generation.LojaGames.Model.Produtos;
 
 @Repository
-public interface ProdutoRepositorio extends JpaRepository<Produtos, Long>{
+public interface ProdutoRepositorio extends JpaRepository<Produtos, Long> {
 
 	/**
 	 * Metodo utilizado para retornar pesquisa pela coluna descricaoTitulo
@@ -18,5 +19,46 @@ public interface ProdutoRepositorio extends JpaRepository<Produtos, Long>{
 	 * @author Gustavo
 	 * @since 1.0
 	 */
-	//public List<Produtos> findAllByDescricaoTituloContainingIgnoreCase(String descricaoTitulo);
+	// public List<Produtos> findAllByContainingIgnoreCase(String descricaoTitulo);
+
+	/**
+	 * Metodo utilizado para retornar pesquisa pela coluna nomeProdutos
+	 * 
+	 * @param nomeProdutos
+	 * @return Lista com todos os produtos
+	 * @author Abner
+	 * @since 1.0
+	 */
+	// public List<Produtos> findAll(Long nomeProdutos);
+
+	/**
+	 * Metodo utilizado para retornar pesquisa pela coluna IdProduto
+	 * 
+	 * @param IdProduto
+	 * @return Optional com o produto do id inserido
+	 * @author Abner
+	 * @since 1.0
+	 */
+	public Optional<Produtos> findById(Long idProduto);
+
+	/**
+	 * Metodo utilizado para retornar pesquisa pela coluna nomeProdutos
+	 * 
+	 * @param nomeProdutos
+	 * @return Lista com nome dos produtos
+	 * @author Abner
+	 * @since 1.0
+	 */
+
+	public List<Produtos> findByNomeProdutosContainingIgnoreCase(String nomeProdutos);
+
+	/**
+	 * Metodo utilizado para retornar pesquisa pela coluna tipoProduto
+	 * 
+	 * @param tipoProduto
+	 * @return Lista com o produto por tipos
+	 * @author Abner
+	 * @since 1.0
+	 */
+	public List<Produtos> findBytipoProduto(String tipoProduto);
 }
