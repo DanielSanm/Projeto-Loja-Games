7a69d6fe96e2239cbe88d943870d7ca4071f3250
abnerwerley <abnerwerley77@gmail.com>
2021-10-07 22:17:52 -0300

    [Feature Abner - Task: UserDetailsImplements feito]

--- src/main/java/com/generation/LojaGames/security/UserDetailsImplements.java
+++ src/main/java/com/generation/LojaGames/security/UserDetailsImplements.java
@@ -1,5 +1,64 @@
 package com.generation.LojaGames.security;
 
-public class UserDetailsImplements {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImplements implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String senha;
+	private List<GrantedAuthority> autorizacoes;
+
+	public UserDetailsImplements(String email, String senha) {
+		// super();
+		this.email = email;
+		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return autorizacoes;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
